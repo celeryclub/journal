6e862ddbb84d60feda3c7a63938363af4914815c
Steve Davis <steve@celery.club>
2017-05-26 18:01:44 +0300

    Import server process from MBG project

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var path = require('path'),
+    express = require('express'),
+    ejs = require('ejs');
+
+var app = express();
+
+app.set('port', 9000);
+app.set('views', __dirname);
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+
+app.use(
+  express.static('public', {
+    setHeaders: function(response) {
+      response.set('Cache-Control', 'public, no-transform, max-age=86400');
+    }
+  })
+);
+
+app.get('*', function(request, response) {
+  response.set('Cache-Control', 'no-cache');
+  response.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
