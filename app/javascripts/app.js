6841620a6e1158de27976f3ab5ce7cf929e126bc
Steve Davis <steve@celery.club>
2017-05-30 18:07:27 +0300

    Set up angular app

---
+++ app/javascripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module(
+  'Lido',
+  ['ng', 'ngRoute']
+).
+config(
+  [
+    '$locationProvider', '$routeProvider',
+    function($locationProvider, $routeProvider) {
+      $routeProvider.when(
+        '/',
+        {
+          controller: 'HomeController',
+          templateUrl: 'templates/home.html'
+        }
+      ).when(
+        '/notes/new',
+        {
+          controller: 'NewController',
+          templateUrl: 'templates/edit.html'
+        }
+      ).when(
+        '/notes/:note_id',
+        {
+          controller: 'ShowController',
+          templateUrl: 'templates/show.html'
+        }
+      ).when(
+        '/notes/:note_id/edit',
+        {
+          controller: 'EditController',
+          templateUrl: 'templates/edit.html'
+        }
+      );
+
+      $locationProvider.html5Mode(true);
+    }
+  ]
+);
