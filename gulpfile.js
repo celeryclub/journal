04878aacaa7fd60da3c93a0c7f6d4d8348d09ed1
Steve Davis <steve@celery.club>
2017-06-28 12:11:30 -0400

    Use development environment as default

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var minimist = require('minimist'),
     options = minimist(process.argv.slice(2), {});
 
-var environment = options.environment || options.e || 'staging';
+var environment = options.environment || options.e || 'development';
 
 var lorry = require('lorry')(environment);
 
