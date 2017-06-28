e30280c208b2223e6d18386ae8507583b52e3222
Steve Davis <steve@celery.club>
2017-06-28 12:01:54 -0400

    Remove server file and use lorry as build tool

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var minimist = require('minimist'),
+    options = minimist(process.argv.slice(2), {});
+
+var environment = options.environment || options.e || 'staging';
+
+var lorry = require('lorry')(environment);
+
+lorry.install('build');
+lorry.install('server');
+lorry.install('deploy');
+
+lorry.setDefault('server');
