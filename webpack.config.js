0b53dfa95f75c9ab0650b40d6a469a61f9a13d69
Steve Davis <steve@celery.club>
2017-11-10 16:21:59 -0500

    Rearrange app and public directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './app/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './public'),
+    publicPath: '/public/',
     filename: 'build.js'
   },
   module: {
