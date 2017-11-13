2411f398617dfbad3b1b56de05b940da80e435f1
Steve Davis <steve@celery.club>
2017-11-12 21:00:25 -0500

    Create Note component

--- app/js/components/Notes.vue
+++ app/js/components/Notes.vue
@@ -1,12 +1,17 @@
 <template>
   <ul>
-    <li v-for="(note, index) in notes">{{ note.name }}</li>
+    <Note v-for="(note, index) in notes" :key="note.id" :note="note" />
   </ul>
 </template>
 
 <script>
+import Note from './Note'
+
 export default {
   name: 'Notes',
+  components: {
+    Note,
+  },
   props: {
     notes: {
       type: Array,
