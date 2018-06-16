6c0717449ee9e0d4794bfc771997f0abe087c54d
Steve Davis <steve@celery.club>
2018-06-15 23:39:43 -0400

    Use VueLoaderPlugin for webpack builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-const APP_PATH = path.resolve(__dirname, './app');
+const APP_PATH = path.resolve(__dirname, './app')
 
 module.exports = {
   entry: './app/js/main.js',
   output: {
     path: path.resolve(__dirname, './public/assets'),
     publicPath: '/public/assets',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   module: {
     rules: [
@@ -16,37 +17,33 @@ module.exports = {
         test: /\.css$/,
         use: [
           'vue-style-loader',
-          'css-loader'
+          'css-loader',
         ],
-      },      {
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
+          name: '[name].[ext]?[hash]',
+        },
+      },
     ]
   },
   resolve: {
     alias: {
       '@': APP_PATH,
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ['*', '.js', '.vue', '.json'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, './public'),
@@ -54,22 +51,22 @@ module.exports = {
     noInfo: true,
     overlay: true,
     port: 9000,
-    disableHostCheck: true
+    disableHostCheck: true,
   },
   performance: {
-    hints: false
+    hints: false,
   },
-  devtool: 'cheap-module-source-map'
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
 }
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = 'source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
